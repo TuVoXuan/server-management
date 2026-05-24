c2fa3c64cbb3d72ffc93a6c2c32edbf4467a13b8
TuVoXuan <voxuantu1235@gmail.com>
2026-05-24 08:29:18 +0700

    fix: fix error build

--- src/pages/infracstructure/locations/index.tsx
+++ src/pages/infracstructure/locations/index.tsx
@@ -1,5 +1,4 @@
 import UnderDevelopmentPage from "@/components/under-development-page";
-import React from "react";
 
 export default function InfrastructureLocationPage() {
   return <UnderDevelopmentPage />;
